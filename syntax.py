37309259d038b1c2256e4fcbcf07b8247ff842e0
VeriteKwizera <116313867+VeriteKwizera@users.noreply.github.com>
2022-11-03 21:59:47 +0000

    guessing game update

--- syntax.py
+++ syntax.py
@@ -1,1 +1,23 @@
-print("Hello World")
+
+# import random
+
+# print(random.randrange(1,6))
+# # print(random.randrange(1,100))      
+# import random, decimal
+# print(decimal.Decimal(random.randrange(10000))/100)
+# print(greeting[2])
+# print(greeting[0])
+
+# print(len(greeting))
+# someTxt = "Red is my favorite color"
+# print("red" in someTxt)
+# def func():
+#   return True
+
+# if func():
+#   print('Yes')
+# else:
+#   print('No')
+# x = 200 
+# print(isinstance(x,str))
+
